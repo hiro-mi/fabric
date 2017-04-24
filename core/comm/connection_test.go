ghaskins/hyperledger-fabric

[FAB-3160] Provide config-relative path feature
Introduction
======================
The primary goal of this patch is to create the notion of a
"config-relative" path reference.  For example, a configuration file
"/etc/foo/bar.yaml" that contains a key "bat" with a value
"baz/blaz/blamo" can be used to specify that "baz/blaz/blamo" should
be considered relative to the configuration file itself.  In this case,
it would be expected to be found at /etc/foo/baz/blaz/blamo.  FAB-2037
does a much more thorough job of explaining the rationale on why
config-relative is considered important/good-form.

This is in stark contrast to what we have today, which is a jumbled
mess of assumed GOPATH relative, CWD relative, ENVVAR absolute and
sometimes even ENVVAR relative.  Therefore, an additional positive
side-effect of this endeavor is that this patch also substantially
cleans up some technical debt that had been accumulating in the tree
for some time related to ad-hoc pathing, DRY violations, and just
general inconsistencies in how configuration files were managed.

Design Details
==========================
This patch refactors the basic configuration system into the notion of
a tree rooted at a configuration-path.  By default, this path is
$GOROOT/..../fabric/sampleconfig during dev/test and
/etc/hyperledger/fabric during runtime.  The root may be overridden
at any time by specifying the environment variable FABRIC_CFG_PATH.
(Note that this variable unifies and replaces the former PEER_CFG_PATH
and ORDERER_CFG_PATH).

The dev/test environment will operate out of the ./fabric/sampleconfig
configuration root.  The build-system will package that root into
/etc/hyperledger/fabric in the runtime context with the intention of
the end-user/admin/deployer replacing parts or all of the sampleconfig
to suit their application.

Since configuration-relative paths are now possible, the configuration
files may reference other relative files and they will behave
appropriately regardless of t

core/comm/connection_test.go
@@ -27,7 +27,7 @@ import (
 )
 
 func TestConnection_Correct(t *testing.T) {
-	config.SetupTestConfig("./../../peer")
+	config.SetupTestConfig()
 	viper.Set("ledger.blockchain.deploy-system-chaincode", "false")
 	peerAddress := GetPeerTestingAddress("7051")
 	var tmpConn *grpc.ClientConn
@@ -44,7 +44,7 @@ func TestConnection_Correct(t *testing.T) {
 }
 
 func TestConnection_WrongAddress(t *testing.T) {
-	config.SetupTestConfig("./../../peer")
+	config.SetupTestConfig()
 	viper.Set("ledger.blockchain.deploy-system-chaincode", "false")
 	peerAddress := GetPeerTestingAddress("7052")
 	var tmpConn *grpc.ClientConn

hiro-mi/fabric

core/comm/connection_test.go
@@ -52,7 +52,7 @@ VQQLDAtIeXBlcmxlZGdlcjESMBAGA1UEAwwJbG9jYWxob3N0MFkwEwYHKoZIzj0C
 `
 
 func TestConnection_Correct(t *testing.T) {
-	testutil.SetupTestConfig("./../../peer")
+	testutil.SetupTestConfig()
 	viper.Set("ledger.blockchain.deploy-system-chaincode", "false")
 	peerAddress := GetPeerTestingAddress("7051")
 	var tmpConn *grpc.ClientConn
@@ -69,7 +69,7 @@ func TestConnection_Correct(t *testing.T) {
 }
 
 func TestConnection_WrongAddress(t *testing.T) {
-	testutil.SetupTestConfig("./../../peer")
+	testutil.SetupTestConfig()
 	viper.Set("ledger.blockchain.deploy-system-chaincode", "false")
 	peerAddress := GetPeerTestingAddress("7052")
 	var tmpConn *grpc.ClientConn