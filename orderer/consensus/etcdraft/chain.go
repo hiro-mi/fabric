adarshsaraf123/hyperledger-fabric

[FAB-11833] Say hello to Raft OSN
This change set:

1) Wires the consenter into the registrar
2) Wires the communication to the consenter and
   to the registrar, in preperation for multi-node Raft.
   This makes a new chain configure the communication layer
   of the cluster according to the TLS certificates
   of the channel.
3) Adds a unit test that spawns a Raft OSN

Change-Id: I3923f6f5e0fae6428c2be2e056b82355332f3324
Signed-off-by: yacovm <yacovm@il.ibm.com>

orderer/consensus/etcdraft/chain.go
@@ -8,19 +8,23 @@ package etcdraft
 
 import (
 	"context"
+	"encoding/pem"
+	"fmt"
 	"sync"
 	"sync/atomic"
 	"time"
 
-	"github.com/hyperledger/fabric/common/flogging"
-	"github.com/hyperledger/fabric/orderer/consensus"
-	"github.com/hyperledger/fabric/protos/common"
-	"github.com/hyperledger/fabric/protos/orderer"
-	"github.com/hyperledger/fabric/protos/utils"
-
 	"code.cloudfoundry.org/clock"
 	"github.com/coreos/etcd/raft"
 	"github.com/coreos/etcd/raft/raftpb"
+	"github.com/golang/protobuf/proto"
+	"github.com/hyperledger/fabric/common/flogging"
+	"github.com/hyperledger/fabric/orderer/common/cluster"
+	"github.com/hyperledger/fabric/orderer/consensus"
+	"github.com/hyperledger/fabric/protos/common"
+	"github.com/hyperledger/fabric/protos/orderer"
+	"github.com/hyperledger/fabric/protos/orderer/etcdraft"
+	"github.com/hyperledger/fabric/protos/utils"
 	"github.com/pkg/errors"
 )
 
@@ -55,7 +59,8 @@ type Options struct {
 
 // Chain implements consensus.Chain interface with raft-based consensus
 type Chain struct {
-	raftID uint64
+	configurator Configurator
+	raftID       uint64
 
 	submitC  chan *orderer.SubmitRequest
 	commitC  chan *common.Block
@@ -80,19 +85,20 @@ type Chain struct {
 }
 
 // NewChain constructs a chain object
-func NewChain(support consensus.ConsenterSupport, opts Options, observe chan<- uint64) (*Chain, error) {
+func NewChain(support consensus.ConsenterSupport, opts Options, observe chan<- uint64, configurator Configurator) (*Chain, error) {
 	return &Chain{
-		raftID:   opts.RaftID,
-		submitC:  make(chan *orderer.SubmitRequest),
-		commitC:  make(chan *common.Block),
-		haltC:    make(chan struct{}),
-		doneC:    make(chan struct{}),
-		observeC: observe,
-		support:  support,
-		clock:    opts.Clock,
-		logger:   opts.Logger.With("channel", support.ChainID()),
-		storage:  opts.Storage,
-		opts:     opts,
+		configurator: configurator,
+		raftID:       opts.RaftID,
+		submitC:      make(chan *orderer.SubmitRequest),
+		commitC:      make(chan *common.Block),
+		haltC:        make(chan struct{}),
+		doneC:        make(chan struct{}),
+		observeC:     observe,
+		support:      support,
+		clock:        opts.Clock,
+		logger:       opts.Logger.With("channel", support.ChainID()),
+		storage:      opts.Storage,
+		opts:         opts,
 	}, nil
 }
 
@@ -109,6 +115,12 @@ func (c *Chain) Start() {
 	}
 
 	c.node = raft.StartNode(config, c.opts.Peers)
+
+	if err := c.configureComm(); err != nil {
+		c.logger.Errorf("Failed starting chain, aborting: +%v", err)
+		close(c.doneC)
+		return
+	}
 
 	go c.serveRaft()
 	go c.serveRequest()
@@ -364,4 +376,53 @@ func (c *Chain) isConfig(env *common.Envelope) bool {
 	}
 
 	return h.Type == int32(common.HeaderType_CONFIG) || h.Type == int32(common.HeaderType_ORDERER_TRANSACTION)
+}
+
+func (c *Chain) configureComm() error {
+	nodes, err := c.nodeConfigFromMetadata()
+	if err != nil {
+		return err
+	}
+	c.configurator.Configure(c.support.ChainID(), nodes)
+	return nil
+}
+
+func (c *Chain) nodeConfigFromMetadata() ([]cluster.RemoteNode, error) {
+	var nodes []cluster.RemoteNode
+	m := &etcdraft.Metadata{}
+	if err := proto.Unmarshal(c.support.SharedConfig().ConsensusMetadata(), m); err != nil {
+		return nil, errors.Wrap(err, "failed extracting consensus metadata")
+	}
+
+	for id, consenter := range m.Consenters {
+		raftID := uint64(id + 1)
+		// No need to know yourself
+		if raftID == c.raftID {
+			continue
+		}
+		serverCertAsDER, err := c.pemToDER(consenter.ServerTlsCert, raftID, "server")
+		if err != nil {
+			return nil, errors.WithStack(err)
+		}
+		clientCertAsDER, err := c.pemToDER(consenter.ClientTlsCert, raftID, "client")
+		if err != nil {
+			return nil, errors.WithStack(err)
+		}
+		nodes = append(nodes, cluster.RemoteNode{
+			ID:            raftID,
+			Endpoint:      fmt.Sprintf("%s:%d", consenter.Host, consenter.Port),
+			ServerTLSCert: serverCertAsDER,
+			ClientTLSCert: clientCertAsDER,
+		})
+	}
+	return nodes, nil
+}
+
+func (c *Chain) pemToDER(pemBytes []byte, id uint64, certType string) ([]byte, error) {
+	bl, _ := pem.Decode(pemBytes)
+	if bl == nil {
+		c.logger.Errorf("Rejecting PEM block of %s TLS cert for node %d, offending PEM is: %s", certType, id, string(pemBytes))
+		return nil, errors.Errorf("invalid PEM block")
+	}
+	return bl.Bytes, nil
 }

orderer/consensus/etcdraft/chain_test.go
@@ -3,24 +3,30 @@ Copyright IBM Corp. All Rights Reserved.
 
 SPDX-License-Identifier: Apache-2.0
 */
+
 package etcdraft_test
 
 import (
+	"encoding/pem"
 	"time"
-
-	"github.com/hyperledger/fabric/common/flogging"
-	mockconfig "github.com/hyperledger/fabric/common/mocks/config"
-	"github.com/hyperledger/fabric/orderer/consensus/etcdraft"
-	consensusmocks "github.com/hyperledger/fabric/orderer/consensus/mocks"
-	mockblockcutter "github.com/hyperledger/fabric/orderer/mocks/common/blockcutter"
-	"github.com/hyperledger/fabric/protos/common"
-	"github.com/hyperledger/fabric/protos/utils"
 
 	"code.cloudfoundry.org/clock/fakeclock"
 	"github.com/coreos/etcd/raft"
+	"github.com/hyperledger/fabric/common/crypto/tlsgen"
+	"github.com/hyperledger/fabric/common/flogging"
+	mockconfig "github.com/hyperledger/fabric/common/mocks/config"
+	"github.com/hyperledger/fabric/orderer/common/cluster"
+	"github.com/hyperledger/fabric/orderer/consensus/etcdraft"
+	"github.com/hyperledger/fabric/orderer/consensus/etcdraft/mocks"
+	consensusmocks "github.com/hyperledger/fabric/orderer/consensus/mocks"
+	mockblockcutter "github.com/hyperledger/fabric/orderer/mocks/common/blockcutter"
+	"github.com/hyperledger/fabric/protos/common"
+	raftprotos "github.com/hyperledger/fabric/protos/orderer/etcdraft"
+	"github.com/hyperledger/fabric/protos/utils"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/pkg/errors"
+	"github.com/stretchr/testify/mock"
 	"go.uber.org/zap"
 )
 
@@ -30,9 +36,11 @@ var _ = Describe("Chain", func() {
 		normalBlock *common.Block
 		interval    time.Duration
 		channelID   string
+		tlsCA       tlsgen.CA
 	)
 
 	BeforeEach(func() {
+		tlsCA, _ = tlsgen.NewCA()
 		channelID = "test-chain"
 		m = &common.Envelope{
 			Payload: utils.MarshalOrPanic(&common.Payload{
@@ -46,14 +54,16 @@ var _ = Describe("Chain", func() {
 
 	Describe("Single raft node", func() {
 		var (
-			clock   *fakeclock.FakeClock
-			opt     etcdraft.Options
-			support *consensusmocks.FakeConsenterSupport
-			cutter  *mockblockcutter.Receiver
-			storage *raft.MemoryStorage
-			observe chan uint64
-			chain   *etcdraft.Chain
-			logger  *flogging.FabricLogger
+			commConfigurator  *mocks.Configurator
+			consenterMetadata *raftprotos.Metadata
+			clock             *fakeclock.FakeClock
+			opt               etcdraft.Options
+			support           *consensusmocks.FakeConsenterSupport
+			cutter            *mockblockcutter.Receiver
+			storage           *raft.MemoryStorage
+			observe           chan uint64
+			chain             *etcdraft.Chain
+			logger            *flogging.FabricLogger
 		)
 
 		campaign := func() {
@@ -70,6 +80,8 @@ var _ = Describe("Chain", func() {
 		}
 
 		BeforeEach(func() {
+			commConfigurator = &mocks.Configurator{}
+			commConfigurator.On("Configure", mock.Anything, mock.Anything)
 			clock = fakeclock.NewFakeClock(time.Now())
 			storage = raft.NewMemoryStorage()
 			logger = flogging.NewFabricLogger(zap.NewNop())
@@ -88,12 +100,16 @@ var _ = Describe("Chain", func() {
 			}
 			support = &consensusmocks.FakeConsenterSupport{}
 			support.ChainIDReturns(channelID)
-			support.SharedConfigReturns(&mockconfig.Orderer{BatchTimeoutVal: time.Hour})
+			consenterMetadata = createMetadata(3, tlsCA)
+			support.SharedConfigReturns(&mockconfig.Orderer{
+				BatchTimeoutVal:      time.Hour,
+				ConsensusMetadataVal: utils.MarshalOrPanic(consenterMetadata),
+			})
 			cutter = mockblockcutter.NewReceiver()
 			support.BlockCutterReturns(cutter)
 
 			var err error
-			chain, err = etcdraft.NewChain(support, opt, observe)
+			chain, err = etcdraft.NewChain(support, opt, observe, commConfigurator)
 			Expect(err).NotTo(HaveOccurred())
 			chain.Start()
 
@@ -117,6 +133,13 @@ var _ = Describe("Chain", func() {
 
 		AfterEach(func() {
 			chain.Halt()
+		})
+
+		Context("when a node starts up", func() {
+			It("properly configures the communication layer", func() {
+				expectedNodeConfig := nodeConfigFromMetadata(consenterMetadata)
+				commConfigurator.AssertCalled(testingInstance, "Configure", channelID, expectedNodeConfig)
+			})
 		})
 
 		Context("when no raft leader is elected", func() {
@@ -332,3 +355,52 @@ var _ = Describe("Chain", func() {
 		})
 	})
 })
+
+func nodeConfigFromMetadata(consenterMetadata *raftprotos.Metadata) []cluster.RemoteNode {
+	var nodes []cluster.RemoteNode
+	for i, consenter := range consenterMetadata.Consenters {
+		// For now, skip ourselves
+		if i == 0 {
+			continue
+		}
+		serverDER, _ := pem.Decode(consenter.ServerTlsCert)
+		clientDER, _ := pem.Decode(consenter.ClientTlsCert)
+		node := cluster.RemoteNode{
+			ID:            uint64(i + 1),
+			Endpoint:      "localhost:7050",
+			ServerTLSCert: serverDER.Bytes,
+			ClientTLSCert: clientDER.Bytes,
+		}
+		nodes = append(nodes, node)
+	}
+	return nodes
+}
+
+func createMetadata(nodeCount int, tlsCA tlsgen.CA) *raftprotos.Metadata {
+	md := &raftprotos.Metadata{}
+	for i := 0; i < nodeCount; i++ {
+		md.Consenters = append(md.Consenters, &raftprotos.Consenter{
+			Host:          "localhost",
+			Port:          7050,
+			ServerTlsCert: serverTLSCert(tlsCA),
+			ClientTlsCert: clientTLSCert(tlsCA),
+		})
+	}
+	return md
+}
+
+func serverTLSCert(tlsCA tlsgen.CA) []byte {
+	cert, err := tlsCA.NewServerCertKeyPair("localhost")
+	if err != nil {
+		panic(err)
+	}
+	return cert.Cert
+}
+
+func clientTLSCert(tlsCA tlsgen.CA) []byte {
+	cert, err := tlsCA.NewClientCertKeyPair()
+	if err != nil {
+		panic(err)
+	}
+	return cert.Cert
+}

hiro-mi/fabric

orderer/consensus/etcdraft/chain.go
@@ -8,19 +8,23 @@ package etcdraft
 
 import (
 	"context"
+	"encoding/pem"
+	"fmt"
 	"sync"
 	"sync/atomic"
 	"time"
 
-	"github.com/hyperledger/fabric/common/flogging"
-	"github.com/hyperledger/fabric/orderer/consensus"
-	"github.com/hyperledger/fabric/protos/common"
-	"github.com/hyperledger/fabric/protos/orderer"
-	"github.com/hyperledger/fabric/protos/utils"
-
 	"code.cloudfoundry.org/clock"
 	"github.com/coreos/etcd/raft"
 	"github.com/coreos/etcd/raft/raftpb"
+	"github.com/golang/protobuf/proto"
+	"github.com/hyperledger/fabric/common/flogging"
+	"github.com/hyperledger/fabric/orderer/common/cluster"
+	"github.com/hyperledger/fabric/orderer/consensus"
+	"github.com/hyperledger/fabric/protos/common"
+	"github.com/hyperledger/fabric/protos/orderer"
+	"github.com/hyperledger/fabric/protos/orderer/etcdraft"
+	"github.com/hyperledger/fabric/protos/utils"
 	"github.com/pkg/errors"
 )
 
@@ -52,7 +56,8 @@ type Options struct {
 
 // Chain implements consensus.Chain interface.
 type Chain struct {
-	raftID uint64
+	communication cluster.Communicator
+	raftID        uint64
 
 	submitC  chan *orderer.SubmitRequest
 	commitC  chan *common.Block
@@ -75,20 +80,21 @@ type Chain struct {
 	logger *flogging.FabricLogger
 }
 
-// NewChain returns a new chain.
-func NewChain(support consensus.ConsenterSupport, opts Options, observe chan<- uint64) (*Chain, error) {
+// NewChain constructs a chain object
+func NewChain(support consensus.ConsenterSupport, opts Options, observe chan<- uint64, comm cluster.Communicator) (*Chain, error) {
 	return &Chain{
-		raftID:   opts.RaftID,
-		submitC:  make(chan *orderer.SubmitRequest),
-		commitC:  make(chan *common.Block),
-		haltC:    make(chan struct{}),
-		doneC:    make(chan struct{}),
-		observeC: observe,
-		support:  support,
-		clock:    opts.Clock,
-		logger:   opts.Logger.With("channel", support.ChainID()),
-		storage:  opts.Storage,
-		opts:     opts,
+		communication: comm,
+		raftID:        opts.RaftID,
+		submitC:       make(chan *orderer.SubmitRequest),
+		commitC:       make(chan *common.Block),
+		haltC:         make(chan struct{}),
+		doneC:         make(chan struct{}),
+		observeC:      observe,
+		support:       support,
+		clock:         opts.Clock,
+		logger:        opts.Logger.With("channel", support.ChainID()),
+		storage:       opts.Storage,
+		opts:          opts,
 	}, nil
 }
 
@@ -102,6 +108,12 @@ func (c *Chain) Start() {
 		MaxInflightMsgs: c.opts.MaxInflightMsgs,
 		Logger:          c.logger,
 		Storage:         c.opts.Storage,
+	}
+
+	if err := c.configureComm(); err != nil {
+		c.logger.Errorf("Failed starting chain, aborting: +%v", err)
+		close(c.doneC)
+		return
 	}
 
 	c.node = raft.StartNode(config, c.opts.Peers)
@@ -360,4 +372,53 @@ func (c *Chain) isConfig(env *common.Envelope) bool {
 	}
 
 	return h.Type == int32(common.HeaderType_CONFIG) || h.Type == int32(common.HeaderType_ORDERER_TRANSACTION)
+}
+
+func (c *Chain) configureComm() error {
+	nodes, err := c.nodeConfigFromMetadata()
+	if err != nil {
+		return err
+	}
+	c.communication.Configure(c.support.ChainID(), nodes)
+	return nil
+}
+
+func (c *Chain) nodeConfigFromMetadata() ([]cluster.RemoteNode, error) {
+	var nodes []cluster.RemoteNode
+	m := &etcdraft.Metadata{}
+	if err := proto.Unmarshal(c.support.SharedConfig().ConsensusMetadata(), m); err != nil {
+		return nil, errors.Wrap(err, "failed extracting consensus metadata")
+	}
+
+	for id, consenter := range m.Consenters {
+		raftID := uint64(id + 1)
+		// No need to know yourself
+		if raftID == c.raftID {
+			continue
+		}
+		serverCertAsDER, err := c.pemToDER(consenter.ServerTlsCert, raftID, "server")
+		if err != nil {
+			return nil, errors.WithStack(err)
+		}
+		clientCertAsDER, err := c.pemToDER(consenter.ClientTlsCert, raftID, "client")
+		if err != nil {
+			return nil, errors.WithStack(err)
+		}
+		nodes = append(nodes, cluster.RemoteNode{
+			ID:            raftID,
+			Endpoint:      fmt.Sprintf("%s:%d", consenter.Host, consenter.Port),
+			ServerTLSCert: serverCertAsDER,
+			ClientTLSCert: clientCertAsDER,
+		})
+	}
+	return nodes, nil
+}
+
+func (c *Chain) pemToDER(pemBytes []byte, id uint64, certType string) ([]byte, error) {
+	bl, _ := pem.Decode(pemBytes)
+	if bl == nil {
+		c.logger.Errorf("Rejecting PEM block of %s TLS cert for node %d, offending PEM is: %s", certType, id, string(pemBytes))
+		return nil, errors.Errorf("invalid PEM block")
+	}
+	return bl.Bytes, nil
 }

orderer/consensus/etcdraft/chain_test.go
@@ -3,9 +3,11 @@ Copyright IBM Corp. All Rights Reserved.
 
 SPDX-License-Identifier: Apache-2.0
 */
+
 package etcdraft_test
 
 import (
+	"encoding/pem"
 	"time"
 
 	. "github.com/onsi/ginkgo"
@@ -13,14 +15,19 @@ import (
 
 	"code.cloudfoundry.org/clock/fakeclock"
 	"github.com/coreos/etcd/raft"
+	"github.com/hyperledger/fabric/common/crypto/tlsgen"
 	"github.com/hyperledger/fabric/common/flogging"
 	mockconfig "github.com/hyperledger/fabric/common/mocks/config"
+	"github.com/hyperledger/fabric/orderer/common/cluster"
+	clustermocks "github.com/hyperledger/fabric/orderer/common/cluster/mocks"
 	"github.com/hyperledger/fabric/orderer/consensus/etcdraft"
 	consensusmocks "github.com/hyperledger/fabric/orderer/consensus/mocks"
 	mockblockcutter "github.com/hyperledger/fabric/orderer/mocks/common/blockcutter"
 	"github.com/hyperledger/fabric/protos/common"
+	raftprotos "github.com/hyperledger/fabric/protos/orderer/etcdraft"
 	"github.com/hyperledger/fabric/protos/utils"
 	"github.com/pkg/errors"
+	"github.com/stretchr/testify/mock"
 	"go.uber.org/zap"
 )
 
@@ -30,9 +37,11 @@ var _ = Describe("Chain", func() {
 		normalBlock *common.Block
 		interval    time.Duration
 		channelID   string
+		tlsCA       tlsgen.CA
 	)
 
 	BeforeEach(func() {
+		tlsCA, _ = tlsgen.NewCA()
 		channelID = "test-chain"
 		m = &common.Envelope{
 			Payload: utils.MarshalOrPanic(&common.Payload{
@@ -46,17 +55,21 @@ var _ = Describe("Chain", func() {
 
 	Describe("Single raft node", func() {
 		var (
-			clock    *fakeclock.FakeClock
-			opts     etcdraft.Options
-			support  *consensusmocks.FakeConsenterSupport
-			cutter   *mockblockcutter.Receiver
-			storage  *raft.MemoryStorage
-			observeC chan uint64
-			chain    *etcdraft.Chain
-			logger   *flogging.FabricLogger
+			comm              *clustermocks.Communicator
+			consenterMetadata *raftprotos.Metadata
+			clock             *fakeclock.FakeClock
+			opts              etcdraft.Options
+			support           *consensusmocks.FakeConsenterSupport
+			cutter            *mockblockcutter.Receiver
+			storage           *raft.MemoryStorage
+			observeC          chan uint64
+			chain             *etcdraft.Chain
+			logger            *flogging.FabricLogger
 		)
 
 		BeforeEach(func() {
+			comm = &clustermocks.Communicator{}
+			comm.On("Configure", mock.Anything, mock.Anything)
 			clock = fakeclock.NewFakeClock(time.Now())
 			storage = raft.NewMemoryStorage()
 			logger = flogging.NewFabricLogger(zap.NewNop())
@@ -75,12 +88,16 @@ var _ = Describe("Chain", func() {
 			}
 			support = &consensusmocks.FakeConsenterSupport{}
 			support.ChainIDReturns(channelID)
-			support.SharedConfigReturns(&mockconfig.Orderer{BatchTimeoutVal: time.Hour})
+			consenterMetadata = createMetadata(3, tlsCA)
+			support.SharedConfigReturns(&mockconfig.Orderer{
+				BatchTimeoutVal:      time.Hour,
+				ConsensusMetadataVal: utils.MarshalOrPanic(consenterMetadata),
+			})
 			cutter = mockblockcutter.NewReceiver()
 			support.BlockCutterReturns(cutter)
 
 			var err error
-			chain, err = etcdraft.NewChain(support, opts, observeC)
+			chain, err = etcdraft.NewChain(support, opts, observeC, comm)
 			Expect(err).NotTo(HaveOccurred())
 		})
 
@@ -107,6 +124,13 @@ var _ = Describe("Chain", func() {
 
 		AfterEach(func() {
 			chain.Halt()
+		})
+
+		Context("when a node starts up", func() {
+			It("properly configures the communication layer", func() {
+				expectedNodeConfig := nodeConfigFromMetadata(consenterMetadata)
+				comm.AssertCalled(testingInstance, "Configure", channelID, expectedNodeConfig)
+			})
 		})
 
 		Context("when no raft leader is elected", func() {
@@ -297,3 +321,52 @@ var _ = Describe("Chain", func() {
 		})
 	})
 })
+
+func nodeConfigFromMetadata(consenterMetadata *raftprotos.Metadata) []cluster.RemoteNode {
+	var nodes []cluster.RemoteNode
+	for i, consenter := range consenterMetadata.Consenters {
+		// For now, skip ourselves
+		if i == 0 {
+			continue
+		}
+		serverDER, _ := pem.Decode(consenter.ServerTlsCert)
+		clientDER, _ := pem.Decode(consenter.ClientTlsCert)
+		node := cluster.RemoteNode{
+			ID:            uint64(i + 1),
+			Endpoint:      "localhost:7050",
+			ServerTLSCert: serverDER.Bytes,
+			ClientTLSCert: clientDER.Bytes,
+		}
+		nodes = append(nodes, node)
+	}
+	return nodes
+}
+
+func createMetadata(nodeCount int, tlsCA tlsgen.CA) *raftprotos.Metadata {
+	md := &raftprotos.Metadata{}
+	for i := 0; i < nodeCount; i++ {
+		md.Consenters = append(md.Consenters, &raftprotos.Consenter{
+			Host:          "localhost",
+			Port:          7050,
+			ServerTlsCert: serverTLSCert(tlsCA),
+			ClientTlsCert: clientTLSCert(tlsCA),
+		})
+	}
+	return md
+}
+
+func serverTLSCert(tlsCA tlsgen.CA) []byte {
+	cert, err := tlsCA.NewServerCertKeyPair("localhost")
+	if err != nil {
+		panic(err)
+	}
+	return cert.Cert
+}
+
+func clientTLSCert(tlsCA tlsgen.CA) []byte {
+	cert, err := tlsCA.NewClientCertKeyPair()
+	if err != nil {
+		panic(err)
+	}
+	return cert.Cert
+}