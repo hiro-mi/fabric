adarshsaraf123/hyperledger-fabric

[FAB-14473] Add additional metrics for etcdraft
This CR adds the following metrics for etcdraft:
	- data_persist_duration
	- # proposals_received

Change-Id: I34b9bdcb3ba7eb0bb074eb7abc93c2f5b5b5c5ad
Signed-off-by: Adarsh Saraf <adarshsaraf123@gmail.com>

orderer/consensus/etcdraft/chain.go
@@ -250,6 +250,8 @@ func NewChain(
 			SnapshotBlockNumber:  opts.Metrics.SnapshotBlockNumber.With("channel", support.ChainID()),
 			LeaderChanges:        opts.Metrics.LeaderChanges.With("channel", support.ChainID()),
 			ProposalFailures:     opts.Metrics.ProposalFailures.With("channel", support.ChainID()),
+			DataPersistDuration:  opts.Metrics.DataPersistDuration.With("channel", support.ChainID()),
+			ProposalsReceived:    opts.Metrics.ProposalsReceived.With("channel", support.ChainID()),
 		},
 		logger:          lg,
 		opts:            opts,
@@ -277,6 +279,7 @@ func NewChain(
 		chainID:      c.channelID,
 		chain:        c,
 		logger:       c.logger,
+		metrics:      c.Metrics,
 		storage:      storage,
 		rpc:          c.rpc,
 		config:       config,
@@ -370,11 +373,13 @@ func (c *Chain) detectMigration() bool {
 
 // Order submits normal type transactions for ordering.
 func (c *Chain) Order(env *common.Envelope, configSeq uint64) error {
+	c.Metrics.ProposalsReceived.Add(1)
 	return c.Submit(&orderer.SubmitRequest{LastValidationSeq: configSeq, Payload: env, Channel: c.channelID}, 0)
 }
 
 // Configure submits config type transactions for ordering.
 func (c *Chain) Configure(env *common.Envelope, configSeq uint64) error {
+	c.Metrics.ProposalsReceived.Add(1)
 	if err := c.checkConfigUpdateValidity(env); err != nil {
 		c.Metrics.ProposalFailures.Add(1)
 		return err

orderer/consensus/etcdraft/metrics.go
@@ -57,6 +57,22 @@ var (
 		LabelNames:   []string{"channel"},
 		StatsdFormat: "%{#fqname}.%{channel}",
 	}
+	dataPersistDurationOpts = metrics.HistogramOpts{
+		Namespace:    "consensus",
+		Subsystem:    "etcdraft",
+		Name:         "data_persist_duration",
+		Help:         "The time taken for etcd/raft data to be persisted in storage.",
+		LabelNames:   []string{"channel"},
+		StatsdFormat: "%{#fqname}.%{channel}",
+	}
+	proposalsReceivedOpts = metrics.CounterOpts{
+		Namespace:    "consensus",
+		Subsystem:    "etcdraft",
+		Name:         "proposals_received",
+		Help:         "The total number of proposals received.",
+		LabelNames:   []string{"channel"},
+		StatsdFormat: "%{#fqname}.%{channel}",
+	}
 )
 
 type Metrics struct {
@@ -66,6 +82,8 @@ type Metrics struct {
 	SnapshotBlockNumber  metrics.Gauge
 	LeaderChanges        metrics.Counter
 	ProposalFailures     metrics.Counter
+	DataPersistDuration  metrics.Histogram
+	ProposalsReceived    metrics.Counter
 }
 
 func NewMetrics(p metrics.Provider) *Metrics {
@@ -76,5 +94,7 @@ func NewMetrics(p metrics.Provider) *Metrics {
 		SnapshotBlockNumber:  p.NewGauge(snapshotBlockNumberOpts),
 		LeaderChanges:        p.NewCounter(leaderChangesOpts),
 		ProposalFailures:     p.NewCounter(proposalFailuresOpts),
+		DataPersistDuration:  p.NewHistogram(dataPersistDurationOpts),
+		ProposalsReceived:    p.NewCounter(proposalsReceivedOpts),
 	}
 }

orderer/consensus/etcdraft/metrics_test.go
@@ -16,18 +16,21 @@ import (
 var _ = Describe("Metrics", func() {
 	Context("NewMetrics", func() {
 		var (
-			fakeProvider *metricsfakes.Provider
-			fakeGauge    *metricsfakes.Gauge
-			fakeCounter  *metricsfakes.Counter
+			fakeProvider  *metricsfakes.Provider
+			fakeGauge     *metricsfakes.Gauge
+			fakeCounter   *metricsfakes.Counter
+			fakeHistogram *metricsfakes.Histogram
 		)
 
 		BeforeEach(func() {
 			fakeProvider = &metricsfakes.Provider{}
 			fakeGauge = &metricsfakes.Gauge{}
 			fakeCounter = &metricsfakes.Counter{}
+			fakeHistogram = &metricsfakes.Histogram{}
 
 			fakeProvider.NewGaugeReturns(fakeGauge)
 			fakeProvider.NewCounterReturns(fakeCounter)
+			fakeProvider.NewHistogramReturns(fakeHistogram)
 		})
 
 		It("uses the provider to initialize a new Metrics object", func() {
@@ -35,7 +38,8 @@ var _ = Describe("Metrics", func() {
 
 			Expect(metrics).NotTo(BeNil())
 			Expect(fakeProvider.NewGaugeCallCount()).To(Equal(4))
-			Expect(fakeProvider.NewCounterCallCount()).To(Equal(2))
+			Expect(fakeProvider.NewCounterCallCount()).To(Equal(3))
+			Expect(fakeProvider.NewHistogramCallCount()).To(Equal(1))
 
 			Expect(metrics.ClusterSize).To(Equal(fakeGauge))
 			Expect(metrics.IsLeader).To(Equal(fakeGauge))
@@ -43,6 +47,63 @@ var _ = Describe("Metrics", func() {
 			Expect(metrics.SnapshotBlockNumber).To(Equal(fakeGauge))
 			Expect(metrics.LeaderChanges).To(Equal(fakeCounter))
 			Expect(metrics.ProposalFailures).To(Equal(fakeCounter))
+			Expect(metrics.DataPersistDuration).To(Equal(fakeHistogram))
+			Expect(metrics.ProposalsReceived).To(Equal(fakeCounter))
 		})
 	})
 })
+
+func newFakeMetrics(fakeFields *fakeMetricsFields) *etcdraft.Metrics {
+	return &etcdraft.Metrics{
+		ClusterSize:          fakeFields.fakeClusterSize,
+		IsLeader:             fakeFields.fakeIsLeader,
+		CommittedBlockNumber: fakeFields.fakeCommittedBlockNumber,
+		SnapshotBlockNumber:  fakeFields.fakeSnapshotBlockNumber,
+		LeaderChanges:        fakeFields.fakeLeaderChanges,
+		ProposalFailures:     fakeFields.fakeProposalFailures,
+		DataPersistDuration:  fakeFields.fakeDataPersistDuration,
+		ProposalsReceived:    fakeFields.fakeProposalsReceived,
+	}
+}
+
+type fakeMetricsFields struct {
+	fakeClusterSize          *metricsfakes.Gauge
+	fakeIsLeader             *metricsfakes.Gauge
+	fakeCommittedBlockNumber *metricsfakes.Gauge
+	fakeSnapshotBlockNumber  *metricsfakes.Gauge
+	fakeLeaderChanges        *metricsfakes.Counter
+	fakeProposalFailures     *metricsfakes.Counter
+	fakeDataPersistDuration  *metricsfakes.Histogram
+	fakeProposalsReceived    *metricsfakes.Counter
+}
+
+func newFakeMetricsFields() *fakeMetricsFields {
+	return &fakeMetricsFields{
+		fakeClusterSize:          newFakeGauge(),
+		fakeIsLeader:             newFakeGauge(),
+		fakeCommittedBlockNumber: newFakeGauge(),
+		fakeSnapshotBlockNumber:  newFakeGauge(),
+		fakeLeaderChanges:        newFakeCounter(),
+		fakeProposalFailures:     newFakeCounter(),
+		fakeDataPersistDuration:  newFakeHistogram(),
+		fakeProposalsReceived:    newFakeCounter(),
+	}
+}
+
+func newFakeGauge() *metricsfakes.Gauge {
+	fakeGauge := &metricsfakes.Gauge{}
+	fakeGauge.WithReturns(fakeGauge)
+	return fakeGauge
+}
+
+func newFakeCounter() *metricsfakes.Counter {
+	fakeCounter := &metricsfakes.Counter{}
+	fakeCounter.WithReturns(fakeCounter)
+	return fakeCounter
+}
+
+func newFakeHistogram() *metricsfakes.Histogram {
+	fakeHistogram := &metricsfakes.Histogram{}
+	fakeHistogram.WithReturns(fakeHistogram)
+	return fakeHistogram
+}

hiro-mi/fabric

	- # normal_proposals_received
	- # config_proposals_received
(cherry picked from commit cca81e5581073a1fad036146b64d9321950c4f3d)

orderer/consensus/etcdraft/chain.go
@@ -255,12 +255,15 @@ func NewChain(
 		createPuller:     f,
 		clock:            opts.Clock,
 		Metrics: &Metrics{
-			ClusterSize:          opts.Metrics.ClusterSize.With("channel", support.ChainID()),
-			IsLeader:             opts.Metrics.IsLeader.With("channel", support.ChainID()),
-			CommittedBlockNumber: opts.Metrics.CommittedBlockNumber.With("channel", support.ChainID()),
-			SnapshotBlockNumber:  opts.Metrics.SnapshotBlockNumber.With("channel", support.ChainID()),
-			LeaderChanges:        opts.Metrics.LeaderChanges.With("channel", support.ChainID()),
-			ProposalFailures:     opts.Metrics.ProposalFailures.With("channel", support.ChainID()),
+			ClusterSize:             opts.Metrics.ClusterSize.With("channel", support.ChainID()),
+			IsLeader:                opts.Metrics.IsLeader.With("channel", support.ChainID()),
+			CommittedBlockNumber:    opts.Metrics.CommittedBlockNumber.With("channel", support.ChainID()),
+			SnapshotBlockNumber:     opts.Metrics.SnapshotBlockNumber.With("channel", support.ChainID()),
+			LeaderChanges:           opts.Metrics.LeaderChanges.With("channel", support.ChainID()),
+			ProposalFailures:        opts.Metrics.ProposalFailures.With("channel", support.ChainID()),
+			DataPersistDuration:     opts.Metrics.DataPersistDuration.With("channel", support.ChainID()),
+			NormalProposalsReceived: opts.Metrics.NormalProposalsReceived.With("channel", support.ChainID()),
+			ConfigProposalsReceived: opts.Metrics.ConfigProposalsReceived.With("channel", support.ChainID()),
 		},
 		logger:          lg,
 		opts:            opts,
@@ -288,6 +291,7 @@ func NewChain(
 		chainID:      c.channelID,
 		chain:        c,
 		logger:       c.logger,
+		metrics:      c.Metrics,
 		storage:      storage,
 		rpc:          c.rpc,
 		config:       config,
@@ -386,11 +390,13 @@ func (c *Chain) detectMigration() bool {
 
 // Order submits normal type transactions for ordering.
 func (c *Chain) Order(env *common.Envelope, configSeq uint64) error {
+	c.Metrics.NormalProposalsReceived.Add(1)
 	return c.Submit(&orderer.SubmitRequest{LastValidationSeq: configSeq, Payload: env, Channel: c.channelID}, 0)
 }
 
 // Configure submits config type transactions for ordering.
 func (c *Chain) Configure(env *common.Envelope, configSeq uint64) error {
+	c.Metrics.ConfigProposalsReceived.Add(1)
 	if err := c.checkConfigUpdateValidity(env); err != nil {
 		c.Metrics.ProposalFailures.Add(1)
 		return err

orderer/consensus/etcdraft/metrics.go
@@ -57,24 +57,54 @@ var (
 		LabelNames:   []string{"channel"},
 		StatsdFormat: "%{#fqname}.%{channel}",
 	}
+	dataPersistDurationOpts = metrics.HistogramOpts{
+		Namespace:    "consensus",
+		Subsystem:    "etcdraft",
+		Name:         "data_persist_duration",
+		Help:         "The time taken for etcd/raft data to be persisted in storage (in seconds).",
+		LabelNames:   []string{"channel"},
+		StatsdFormat: "%{#fqname}.%{channel}",
+	}
+	normalProposalsReceivedOpts = metrics.CounterOpts{
+		Namespace:    "consensus",
+		Subsystem:    "etcdraft",
+		Name:         "normal_proposals_received",
+		Help:         "The total number of proposals received for normal type transactions.",
+		LabelNames:   []string{"channel"},
+		StatsdFormat: "%{#fqname}.%{channel}",
+	}
+	configProposalsReceivedOpts = metrics.CounterOpts{
+		Namespace:    "consensus",
+		Subsystem:    "etcdraft",
+		Name:         "config_proposals_received",
+		Help:         "The total number of proposals received for config type transactions.",
+		LabelNames:   []string{"channel"},
+		StatsdFormat: "%{#fqname}.%{channel}",
+	}
 )
 
 type Metrics struct {
-	ClusterSize          metrics.Gauge
-	IsLeader             metrics.Gauge
-	CommittedBlockNumber metrics.Gauge
-	SnapshotBlockNumber  metrics.Gauge
-	LeaderChanges        metrics.Counter
-	ProposalFailures     metrics.Counter
+	ClusterSize             metrics.Gauge
+	IsLeader                metrics.Gauge
+	CommittedBlockNumber    metrics.Gauge
+	SnapshotBlockNumber     metrics.Gauge
+	LeaderChanges           metrics.Counter
+	ProposalFailures        metrics.Counter
+	DataPersistDuration     metrics.Histogram
+	NormalProposalsReceived metrics.Counter
+	ConfigProposalsReceived metrics.Counter
 }
 
 func NewMetrics(p metrics.Provider) *Metrics {
 	return &Metrics{
-		ClusterSize:          p.NewGauge(clusterSizeOpts),
-		IsLeader:             p.NewGauge(isLeaderOpts),
-		CommittedBlockNumber: p.NewGauge(committedBlockNumberOpts),
-		SnapshotBlockNumber:  p.NewGauge(snapshotBlockNumberOpts),
-		LeaderChanges:        p.NewCounter(leaderChangesOpts),
-		ProposalFailures:     p.NewCounter(proposalFailuresOpts),
+		ClusterSize:             p.NewGauge(clusterSizeOpts),
+		IsLeader:                p.NewGauge(isLeaderOpts),
+		CommittedBlockNumber:    p.NewGauge(committedBlockNumberOpts),
+		SnapshotBlockNumber:     p.NewGauge(snapshotBlockNumberOpts),
+		LeaderChanges:           p.NewCounter(leaderChangesOpts),
+		ProposalFailures:        p.NewCounter(proposalFailuresOpts),
+		DataPersistDuration:     p.NewHistogram(dataPersistDurationOpts),
+		NormalProposalsReceived: p.NewCounter(normalProposalsReceivedOpts),
+		ConfigProposalsReceived: p.NewCounter(configProposalsReceivedOpts),
 	}
 }

orderer/consensus/etcdraft/metrics_test.go
@@ -16,18 +16,21 @@ import (
 var _ = Describe("Metrics", func() {
 	Context("NewMetrics", func() {
 		var (
-			fakeProvider *metricsfakes.Provider
-			fakeGauge    *metricsfakes.Gauge
-			fakeCounter  *metricsfakes.Counter
+			fakeProvider  *metricsfakes.Provider
+			fakeGauge     *metricsfakes.Gauge
+			fakeCounter   *metricsfakes.Counter
+			fakeHistogram *metricsfakes.Histogram
 		)
 
 		BeforeEach(func() {
 			fakeProvider = &metricsfakes.Provider{}
 			fakeGauge = &metricsfakes.Gauge{}
 			fakeCounter = &metricsfakes.Counter{}
+			fakeHistogram = &metricsfakes.Histogram{}
 
 			fakeProvider.NewGaugeReturns(fakeGauge)
 			fakeProvider.NewCounterReturns(fakeCounter)
+			fakeProvider.NewHistogramReturns(fakeHistogram)
 		})
 
 		It("uses the provider to initialize a new Metrics object", func() {
@@ -35,7 +38,8 @@ var _ = Describe("Metrics", func() {
 
 			Expect(metrics).NotTo(BeNil())
 			Expect(fakeProvider.NewGaugeCallCount()).To(Equal(4))
-			Expect(fakeProvider.NewCounterCallCount()).To(Equal(2))
+			Expect(fakeProvider.NewCounterCallCount()).To(Equal(4))
+			Expect(fakeProvider.NewHistogramCallCount()).To(Equal(1))
 
 			Expect(metrics.ClusterSize).To(Equal(fakeGauge))
 			Expect(metrics.IsLeader).To(Equal(fakeGauge))
@@ -43,6 +47,67 @@ var _ = Describe("Metrics", func() {
 			Expect(metrics.SnapshotBlockNumber).To(Equal(fakeGauge))
 			Expect(metrics.LeaderChanges).To(Equal(fakeCounter))
 			Expect(metrics.ProposalFailures).To(Equal(fakeCounter))
+			Expect(metrics.DataPersistDuration).To(Equal(fakeHistogram))
+			Expect(metrics.NormalProposalsReceived).To(Equal(fakeCounter))
+			Expect(metrics.ConfigProposalsReceived).To(Equal(fakeCounter))
 		})
 	})
 })
+
+func newFakeMetrics(fakeFields *fakeMetricsFields) *etcdraft.Metrics {
+	return &etcdraft.Metrics{
+		ClusterSize:             fakeFields.fakeClusterSize,
+		IsLeader:                fakeFields.fakeIsLeader,
+		CommittedBlockNumber:    fakeFields.fakeCommittedBlockNumber,
+		SnapshotBlockNumber:     fakeFields.fakeSnapshotBlockNumber,
+		LeaderChanges:           fakeFields.fakeLeaderChanges,
+		ProposalFailures:        fakeFields.fakeProposalFailures,
+		DataPersistDuration:     fakeFields.fakeDataPersistDuration,
+		NormalProposalsReceived: fakeFields.fakeNormalProposalsReceived,
+		ConfigProposalsReceived: fakeFields.fakeConfigProposalsReceived,
+	}
+}
+
+type fakeMetricsFields struct {
+	fakeClusterSize             *metricsfakes.Gauge
+	fakeIsLeader                *metricsfakes.Gauge
+	fakeCommittedBlockNumber    *metricsfakes.Gauge
+	fakeSnapshotBlockNumber     *metricsfakes.Gauge
+	fakeLeaderChanges           *metricsfakes.Counter
+	fakeProposalFailures        *metricsfakes.Counter
+	fakeDataPersistDuration     *metricsfakes.Histogram
+	fakeNormalProposalsReceived *metricsfakes.Counter
+	fakeConfigProposalsReceived *metricsfakes.Counter
+}
+
+func newFakeMetricsFields() *fakeMetricsFields {
+	return &fakeMetricsFields{
+		fakeClusterSize:             newFakeGauge(),
+		fakeIsLeader:                newFakeGauge(),
+		fakeCommittedBlockNumber:    newFakeGauge(),
+		fakeSnapshotBlockNumber:     newFakeGauge(),
+		fakeLeaderChanges:           newFakeCounter(),
+		fakeProposalFailures:        newFakeCounter(),
+		fakeDataPersistDuration:     newFakeHistogram(),
+		fakeNormalProposalsReceived: newFakeCounter(),
+		fakeConfigProposalsReceived: newFakeCounter(),
+	}
+}
+
+func newFakeGauge() *metricsfakes.Gauge {
+	fakeGauge := &metricsfakes.Gauge{}
+	fakeGauge.WithReturns(fakeGauge)
+	return fakeGauge
+}
+
+func newFakeCounter() *metricsfakes.Counter {
+	fakeCounter := &metricsfakes.Counter{}
+	fakeCounter.WithReturns(fakeCounter)
+	return fakeCounter
+}
+
+func newFakeHistogram() *metricsfakes.Histogram {
+	fakeHistogram := &metricsfakes.Histogram{}
+	fakeHistogram.WithReturns(fakeHistogram)
+	return fakeHistogram
+}